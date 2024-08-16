e645df49d4a75c4ff841a3eab7e8b78b9f7bca78
Kar_Yan <102352076+Karlenjak@users.noreply.github.com>
2024-08-17 00:17:41 +0200

    adding new proyect weMasterKH

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -32,7 +32,10 @@ import CvKarlen from '../images/cv-karlen.png'
 import LaravelReact from '../images/laravel-react.png'
 import SirunikThemeWordpress from '../images/SirunikThemeWordpress.png'
 import Car3DModel from '../images/car.png'
+import webMasterKH from '../images/webMasterKH.png'
+
 const images = [
+  webMasterKH,
   Gandz,
   reformaConstruction,
   HayPoli,
@@ -48,10 +51,18 @@ const images = [
 const Carousel = () => {
   const projects = [
     {
+      image: webMasterKH,
+      githubLink: 'https://github.com/Karlen-yan/my-portfolio_Karlen',
+      onlineLink: 'https://web-master-kh.vercel.app/',
+      technologies: [faJs, faReact, faCss3Alt],
+      description:
+        'A platform that centralizes essential tools and resources for developing and learning about web technologies, making both work and study in web development easier.'
+    },
+    {
       image: Gandz,
       githubLink: 'https://github.com/Karlen-yan/iglesia-gandz',
       onlineLink: '',
-      technologies: [faJs,faReact, faCss3Alt, faNode],
+      technologies: [faJs, faReact, faCss3Alt, faNode],
       description:
         "This ongoing project involves the creation of a multilingual website for a church. The site is designed to be accessible in multiple languages, providing information about the church's services, events, and activities"
     },
@@ -59,7 +70,7 @@ const Carousel = () => {
       image: reformaConstruction,
       githubLink: 'https://github.com/Karlen-yan/reforma-mantenimiento',
       onlineLink: '',
-      technologies: [faJs,faReact, faCss3Alt, faNode],
+      technologies: [faJs, faReact, faCss3Alt, faNode],
       description:
         'A website for air conditioning installation services, featuring service presentation, completed projects, and an interactive contact form.'
     },
@@ -104,13 +115,13 @@ const Carousel = () => {
     },
     {
       image: Cobeer,
-      technologies: [faPhp,faJs, faCss3Alt]
+      technologies: [faPhp, faJs, faCss3Alt]
       // githubLink: '',
       // description:"",
     },
     {
       image: CvKarlen,
-      technologies: [faJs,faReact, faCss3Alt],
+      technologies: [faJs, faReact, faCss3Alt],
       githubLink: 'https://github.com/Karlen-yan/KarlenCV__',
       description:
         'Digital CV Project: Created with the purpose of having an interactive and dynamic online curriculum vitae. This project allows users to download their CV as it appears on the screen. Additionally, all pages are available in both English and Spanish to reach a broader audience. The main objective is to provide a platform where the description and personal information can be completed. Through this project, the aim is also to practice new technologies, such as Next.js in this case, to gain knowledge in modern web development.'

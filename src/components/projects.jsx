d339ab2d44b637aa2853472f5a976cb1ff0b1728
Kar_Yan <102352076+Karlenjak@users.noreply.github.com>
2024-09-25 00:46:22 +0200

    change of card to Card

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next"; 
 import Container from "./shared/container";
-import ProjectCards from "./cardsProjects";
+import ProjectCards from "./CardsProjects";
 
 const Projects = () => {
 
